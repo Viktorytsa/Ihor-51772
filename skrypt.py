5469db9e207c2353a4e673ca9e3df5dead072fe8
Viktorytsa <161588383+Viktorytsa@users.noreply.github.com>
2024-05-23 15:48:35 +0200

    Update skrypt.py

--- skrypt.py
+++ skrypt.py
@@ -1,5 +1,5 @@
 import argparse
-import json
+import yaml
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Program do konwersji danych obsługujący formaty .xml, .json i .yml (.yaml)")
@@ -7,41 +7,28 @@ def parse_arguments():
     parser.add_argument("output_file", help="Ścieżka do pliku wyjściowego")
     return parser.parse_args()
 
-def load_json(file_path):
+def load_yaml(file_path):
     try:
         with open(file_path, 'r') as file:
-            data = json.load(file)
+            data = yaml.safe_load(file)
         return data
-    except json.JSONDecodeError as e:
-        print(f"Błąd podczas wczytywania pliku JSON: {e}")
+    except yaml.YAMLError as e:
+        print(f"Błąd podczas wczytywania pliku YAML: {e}")
         return None
     except FileNotFoundError:
         print("Plik nie istnieje.")
         return None
-
-def save_as_json(data, file_path):
-    try:
-        with open(file_path, 'w') as file:
-            json.dump(data, file, indent=4)
-        print(f"Dane zapisane do pliku {file_path} w formacie JSON.")
-    except Exception as e:
-        print(f"Wystąpił błąd podczas zapisu danych do pliku JSON: {e}")
 
 def main():
     args = parse_arguments()
     input_file = args.input_file
     output_file = args.output_file
 
-    # Wczytywanie danych z pliku JSON
-    data = load_json(input_file)
+    # Wczytywanie danych z pliku YAML
+    data = load_yaml(input_file)
     if data:
         print("Dane wczytane poprawnie.")
         # Tutaj możesz kontynuować pracę z wczytanymi danymi
-
-        # Zapis danych do pliku JSON
-        save_as_json(data, output_file)
-    else:
-        print("Wystąpił błąd podczas wczytywania danych.")
 
 if __name__ == "__main__":
     main()
