57eada9a9a3d05c9d798b17c7dcc2516d977cd80
Viktorytsa <161588383+Viktorytsa@users.noreply.github.com>
2024-05-23 15:41:23 +0200

    Update skrypt.py

--- skrypt.py
+++ skrypt.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Program do konwersji danych obsługujący formaty .xml, .json i .yml (.yaml)")
@@ -6,12 +7,31 @@ def parse_arguments():
     parser.add_argument("output_file", help="Ścieżka do pliku wyjściowego")
     return parser.parse_args()
 
+def load_json(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return data
+    except json.JSONDecodeError as e:
+        print(f"Błąd podczas wczytywania pliku JSON: {e}")
+        return None
+    except FileNotFoundError:
+        print("Plik nie istnieje.")
+        return None
+
 def main():
     args = parse_arguments()
     input_file = args.input_file
     output_file = args.output_file
 
-    # Tutaj możesz kontynuować pracę z argumentami
+    # Wczytywanie danych z pliku JSON
+    data = load_json(input_file)
+    if data:
+        print("Dane wczytane poprawnie.")
+        # Tutaj możesz kontynuować pracę z wczytanymi danymi
+    else:
+        print("Wystąpił błąd podczas wczytywania danych.")
 
 if __name__ == "__main__":
     main()
+
